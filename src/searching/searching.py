247cd3b0007610a6958748ec6354c4ba642e0960
Alexander Besse <alexanderjoeb@hotmail.com>
2020-06-15 18:46:02 -0500

    Sorting and Search completed.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,28 @@
+import math
+
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
+    for i in range(0, len(arr), 1):
+        item = arr[i]
+        if item == target:
+            return i
+        else: continue
+    return -1
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
 
-    # Your code here
+    while start <= end:
+        middle = start + (end - start) // 2
+        middle_value = arr[middle]
 
+        if middle_value == target:
+            return middle
+        elif target < middle_value:
+            end = middle - 1
+        else:
+            start = middle + 1
 
-    return -1  # not found
+    return -1
